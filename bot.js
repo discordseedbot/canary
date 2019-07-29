cedba62305ab27f8d214b385a2588107104c034d
Jyles Coad-Ward <jyles@dariox.club>
2019-07-29 09:29:10 +0800

    Added console indicater for what branch the bot is running. Added .gitignore

--- bot.js
+++ bot.js
@@ -19,12 +19,13 @@ const { Signale } = require('signale');
 const signal = new Signale(options);
 const config = require("./config.json"); const package = require('./package.json'); const build = package.build; const ver = package.version; const ownerID = package.ownerID; const ytapi = config.ytApiToken;
 
+//Check what branch SeedBot is running on
+if (package.branch == "canary") {var branch = "Canary";}if (package.branch === "stable") {var branch = "Stable";}
 
-
-
-signal.info("Starting SeedBot...")
-signal.info("Copyright 2018-2019, jyles.club and DARiOX.club")
-signal.info("Running SeedBot version " + ver + " build " + build);
+signal.info("Starting SeedBot...");
+signal.info("Copyright 2018-2019, jyles.club and DARiOX.club");
+signal.info("Branch: " + branch);
+signal.info('Running SeedBot ' + branch + " (" + ver + ", build" + build +")");
 
 //Math command
 client.on('message',async message=>{if(message.author.bot)return;if(message.content.indexOf(config.mathprefix)!==0)return;const args=message.content.slice(config.mathprefix.length).trim().split(/ +/g);const devcommand=args.shift().toLowerCase();if(message.content.startsWith(config.mathprefix)){let calculate="="+message.content.toLowerCase().substring(config.mathprefix.length);if(isFinite(calculate.replace(/\=|\+|\-|\*|\/|\÷|\%|\(|\)|\,|\ |math.|pow|sqrt|round|floor|ceiling|ceil|pi|π|euler|absolute|abs|exp|logarithm|log|random|rand|rng/g,''))){calculate=calculate.replace(/ /g,"").replace(/÷/g,"/").replace(/power|pow/g,"Math.pow").replace(/sqrt|squareroot/g,"Math.sqrt").replace(/round/g,"Math.round").replace(/floor/g,"Math.floor").replace(/ceiling|ceil/g,"Math.ceil").replace(/pi|π/g,"Math.PI").replace(/euler/g,"Math.E").replace(/absolute|abs/g,"Math.abs").replace(/exp/g,"Math.exp").replace(/logarithm|log/g,"Math.log").replace(/random|rand|rng/g,"Math.random()");if(calculate.replace(/[^%]/g,"").length>0){for(let i=0;i<calculate.replace(/[^%]/g,"").length;i++){while((calculate[getSubstringIndex(calculate,"%",i+1)+1]=="+"||calculate[getSubstringIndex(calculate,"%",i+1)+1]=="-"||calculate[getSubstringIndex(calculate,"%",i+1)+1]=="*"||calculate[getSubstringIndex(calculate,"%",i+1)+1]=="/"||calculate[getSubstringIndex(calculate,"%",i+1)+1]=="("||calculate[getSubstringIndex(calculate,"%",i+1)+1]==")"||calculate[getSubstringIndex(calculate,"%",i+1)+1]==","||getSubstringIndex(calculate,"%",i+1)+1==calculate.length)&&calculate.replace(/[^%]/g,"").length>0){for(let j=getSubstringIndex(calculate,"%",i+1);j>-1;j--){if(calculate[j]=="="||calculate[j]=="+"||calculate[j]=="-"||calculate[j]=="*"||calculate[j]=="/"||calculate[j]=="("||calculate[j]==")"||calculate[j]==","){calculate=calculate.substring(0,j+1)+(calculate.substring(j+1,getSubstringIndex(calculate,"%",i+1))/100)+calculate.substring(getSubstringIndex(calculate,"%",i+1)+1,calculate.length);break}}}}}calculate=calculate.replace(/=/g,"");if(isFinite(eval(calculate)))message.channel.send(eval(calculate));console.log(eval(calculate))}}})
