0750fc3b92bbecd1536628850cff342f07140c34
Jyles Coad-Ward <jyles@dariox.club>
2019-08-12 20:25:48 +0800

    fix

--- bot.js
+++ bot.js
@@ -340,7 +340,7 @@ client.on("message", async message => {
 		author: {name: 's!patreon'},
 		fields: [{
 			name: 'Patreon',
-			value: "It's not free to run a discord bot and since more people are starting to use SeedBot I made a patreon page \n so people can give me money to run my discord bot so it can handle more people at once (which is what it is struggling at), also so when we get more money I can hire someone to make modules for the bot and in the future make a dashboard so you have a better experience.\n\n If you want all of these good things that other bots have then Donate to our patreon page at:\n https://patreon.com/jyles_coadward",\
+			value: "It's not free to run a discord bot and since more people are starting to use SeedBot I made a patreon page \n so people can give me money to run my discord bot so it can handle more people at once (which is what it is struggling at), also so when we get more money I can hire someone to make modules for the bot and in the future make a dashboard so you have a better experience.\n\n If you want all of these good things that other bots have then Donate to our patreon page at:\n https://patreon.com/jyles_coadward",
 		}],
 		timestamp: 'Command Requested at ' + new Date(),
 		footer: {
