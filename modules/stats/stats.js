0a96d41f2d89635cb526394e357f3c8724c9fd51
jylescoad-ward <darioxdotclub@gmail.com>
2019-10-23 09:39:10 +0800

    Added the rest of requests.

--- modules/stats/stats.js
+++ modules/stats/stats.js
@@ -3,6 +3,7 @@ const { RichEmbed } = require("discord.js");
 const client = new Discord.Client();
 const token = require("./../../token.json");
 const prefix = require("./../../prefix.json").default;
+const package = require("./../../package.json");
 
 module.exports.cmd = function() {
 	client.on('message',async message => {
@@ -21,6 +22,86 @@ module.exports.cmd = function() {
 							.setTitle("SeedBot User Count")
 							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
 						message.channel.send(userCountResponse);
+						break;
+					case 'guildCount':
+						var response = client.guilds.size;
+						var gulidCountResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Guild Count")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(gulidCountResponse);
+						break;
+					case 'channelCount':
+						var response = client.channels.size;
+						var channelCountResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Channel Count")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(channelCountResponse);
+						break;
+					case 'botVersion':
+						var response = package.version;
+						var botVerResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Version")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(botVerResponse);
+						break;
+					case 'botBuild':
+						var response = package.build[0];
+						var botBuildResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Bot Build")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(botBuildResponse);
+						break;
+					case 'botBuildDate':
+						var response = package.build[1];
+						var buildDateResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Build Date")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(buildDateResponse);
+						break;
+					case 'botBranch':
+						var response = package.branch;
+						var branchResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Branch")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(branchResponse);
+						break;
+					case 'botOwnerID':
+						var response = package.ownerID;
+						var botOwnerIDResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Owner ID")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(botOwnerIDResponse);
+						break;
+					case 'packageName':
+						var response = package.name;
+						var pkgNameResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Package Name")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(pkgNameResponse);
+						break;
+					case 'botLicense':
+						var response = package.license;
+						var licenseResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot License")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(licenseResponse);
+						break;
+					case 'packageDescription':
+						var response = package.description;
+						var pkgDescResponse = new Discord.RichEmbed()
+							.setColor(require("./../functions/main.js").randomhexcolor())
+							.setTitle("SeedBot Package Description")
+							.setDescription("Request:\n ```\n"+request+"```\n\nResponse: \n```"+response+"\n```")
+						message.channel.send(pkgDescResponse);
 						break;
 					default:
 						var defaultResponse = new Discord.Richembed()
