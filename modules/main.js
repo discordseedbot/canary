7f5f3fd29633bf60a5071d763963deec6ac4939b
jylescoad-ward <jylescoad-ward@DESKTOP.local>
2019-11-13 17:17:19 +0800

    fixed timing

--- modules/main.js
+++ modules/main.js
@@ -16,7 +16,7 @@ module.exports.init = function () {
 				require("./functions/console.js").debug(`Bot has started, with ` + client.users.size + ` users, in ` + client.channels.size + ` channels of ` + client.guilds.size + ` guilds.`);
 				client.user.setActivity(`Sound of Sorting - s!help`, { type: 'LISTENING'});
     			client.user.setStatus('online', "SeedBot Online");
-			},130000)
+			},90000)
 		});
 	client.login(token.discord);
 
